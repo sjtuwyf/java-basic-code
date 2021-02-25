df1fdf7d6bc7885de97ab59971f56f11a20e72f0
ssqswyf <sjtuwyf@126.com>
2021-02-25 20:09:32 +0800

    java语言基础练习，部分进阶练习

---
+++ lianxiti/src/Math/Test1.java
@@ -0,0 +1,38 @@
+package Math;
+
+import java.util.Scanner;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+        System.out.println(next);
+        String s = reverseStr(next);
+        System.out.println(s);
+//        System.out.println(reverseStr(next));
+//        for (int i = 0; i < next.length(); i++) {
+//            System.out.print(reverseStr(next).toCharArray()[i]);
+//        }
+    }
+
+    public static String reverseStr(String str){
+//        String s = new String();
+//        System.out.println(str);
+        System.out.println(str);
+        char[] chars = str.toCharArray();
+        System.out.println(chars);
+//        System.out.println(chars);
+        for (int i = 0; i < chars.length-1-i; i++) {
+//            if(i>=chars.length-1-i){
+//                break;
+//            }
+            char c=chars[i];
+            chars[i]=chars[chars.length-1-i];
+            chars[chars.length-1-i]=c;
+
+        }
+
+        return new String(chars);
+    }
+
+}
