35f87ceaea143c410224e5c7178687eb194e48bc
ssqswyf <sjtuwyf@126.com>
2021-02-26 19:14:20 +0800

    java语言基础练习 2021/2/26 下午 缓冲流

--- shuangyuan/10_IO/src/com/itheima/demo03/ReverseStream/Demo01FileReader.java
+++ shuangyuan/10_IO/src/com/itheima/demo03/ReverseStream/Demo01FileReader.java
@@ -1,8 +1,17 @@
 package com.itheima.demo03.ReverseStream;
 
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Demo01FileReader {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("shuangyuan\\10_IO\\我是GBK格式的文本.txt");
+        int len =0;
+        while ((len = fr.read())!=-1){
+            System.out.println(/*(char)*/len);
+        }
+        fr.close();
     }
 }
